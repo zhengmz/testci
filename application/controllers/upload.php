d4e99278b270cb068b550df820ea94f615a1af9a
zhengmz-centos6 <zhengmz@139.com>
2013-12-14 20:17:28 +0800

    modified:   upload.php

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -15,7 +15,7 @@ class Upload extends CI_Controller {
 
  function do_upload()
  {
-  $config['upload_path'] = '../uploads/';
+  $config['upload_path'] = 'uploads/';
   $config['allowed_types'] = 'gif|jpg|png';
   $config['max_size'] = '100';
   $config['max_width']  = '1024';
