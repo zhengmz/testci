8d93cf0d128e37f61a6ea4a8e30f244449b357b6
zhengmz-cygwin <zhengmz@hotmail.com>
2013-12-19 22:53:46 +0800

    update blog.php: add user-agent

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -19,6 +19,29 @@ class Blog extends CI_Controller {
 			    );
 
 		$this->parser->parse('blog_template', $data);
+
+$this->load->library('user_agent');
+
+if ($this->agent->is_browser())
+{
+    $agent = $this->agent->browser().' '.$this->agent->version();
+}
+elseif ($this->agent->is_robot())
+{
+    $agent = $this->agent->robot();
+}
+elseif ($this->agent->is_mobile())
+{
+    $agent = $this->agent->mobile();
+}
+else
+{
+    $agent = 'Unidentified User Agent';
+}
+
+echo $agent;
+
+echo $this->agent->platform();
 	}
 
 	function news()
