b5c6c1f8096c907f1cd79f7582a72ad2d6558918
zhengmz-cygwin <zhengmz@hotmail.com>
2013-12-19 22:51:51 +0800

    update blog.php

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -2,46 +2,20 @@
 
 class Blog extends CI_Controller {
  
-	function index($blog_entries = '')
+	function index()
 	{
-		$this->load->library('user_agent');
-		if ($this->agent->is_browser())
-		{
-		    $agent = $this->agent->browser().' '.$this->agent->version();
-		}
-		elseif ($this->agent->is_robot())
-		{
-		    $agent = $this->agent->robot();
-		}
-		elseif ($this->agent->is_mobile())
-		{
-		    $agent = $this->agent->mobile();
-		}
-		else
-		{
-		    $agent = 'Unidentified User Agent';
-		}
-
-		echo $agent;
-
-		echo $this->agent->platform();
-
 		$this->load->library('parser');
 
-		if ($blog_entries == '')
-		{
-			$blog_entries = array(
+		$data = array(
+			    'blog_title' => 'My Blog Title',
+			    'blog_heading' => 'My Blog Heading',
+			    'blog_entries' => array(
 				array('title' => 'Title 1', 'body' => 'Body 1'),
 				array('title' => 'Title 2', 'body' => 'Body 2'),
 				array('title' => 'Title 3', 'body' => 'Body 3'),
 				array('title' => 'Title 4', 'body' => 'Body 4'),
 				array('title' => 'Title 5', 'body' => 'Body 5')
-			)
-		}
-		$data = array(
-			    'blog_title' => 'My Blog Title',
-			    'blog_heading' => 'My Blog Heading',
-			    'blog_entries' => $blog_entries
+                                      )
 			    );
 
 		$this->parser->parse('blog_template', $data);
@@ -51,7 +25,16 @@ class Blog extends CI_Controller {
 	{
     		$this->load->database();
 		$query = $this->db->query("SELECT title, text as body FROM news");
-		$this->index($query->result_array());
+
+		$this->load->library('parser');
+
+		$data = array(
+			      'blog_title'   => 'My Blog Title',
+			      'blog_heading' => 'My Blog Heading',
+			      'blog_entries' => $query->result_array()
+			    );
+
+		$this->parser->parse('blog_template', $data);
 	}
 }
 
