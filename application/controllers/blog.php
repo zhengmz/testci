253d7a71674bc0ec3576fa68f5f78e154383a20d
zhengmz-cygwin <zhengmz@hotmail.com>
2013-12-19 22:38:06 +0800

    update blog.php

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -4,26 +4,6 @@ class Blog extends CI_Controller {
  
 	function index($blog_entries = '')
 	{
-		$this->load->library('parser');
-
-		if ($blog_entries == '')
-		{
-			$blog_entries = array(
-				array('title' => 'Title 1', 'body' => 'Body 1'),
-				array('title' => 'Title 2', 'body' => 'Body 2'),
-				array('title' => 'Title 3', 'body' => 'Body 3'),
-				array('title' => 'Title 4', 'body' => 'Body 4'),
-				array('title' => 'Title 5', 'body' => 'Body 5')
-			)
-		}
-		$data = array(
-			    'blog_title' => 'My Blog Title',
-			    'blog_heading' => 'My Blog Heading',
-			    'blog_entries' => $blog_entries
-			    );
-
-		$this->parser->parse('blog_template', $data);
-
 		$this->load->library('user_agent');
 		if ($this->agent->is_browser())
 		{
@@ -44,7 +24,27 @@ class Blog extends CI_Controller {
 
 		echo $agent;
 
-		echo $this->agent->platform(); // Platform info (Windows, Linux, Mac, etc.)
+		echo $this->agent->platform();
+
+		$this->load->library('parser');
+
+		if ($blog_entries == '')
+		{
+			$blog_entries = array(
+				array('title' => 'Title 1', 'body' => 'Body 1'),
+				array('title' => 'Title 2', 'body' => 'Body 2'),
+				array('title' => 'Title 3', 'body' => 'Body 3'),
+				array('title' => 'Title 4', 'body' => 'Body 4'),
+				array('title' => 'Title 5', 'body' => 'Body 5')
+			)
+		}
+		$data = array(
+			    'blog_title' => 'My Blog Title',
+			    'blog_heading' => 'My Blog Heading',
+			    'blog_entries' => $blog_entries
+			    );
+
+		$this->parser->parse('blog_template', $data);
 	}
 
 	function news()
