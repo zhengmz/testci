36fcc377d5e3be915a6f63425463f3e8f1f1567a
zhengmz-cygwin <zhengmz@hotmail.com>
2013-12-19 23:45:10 +0800

    testing zip

---
+++ application/controllers/zip.php
@@ -0,0 +1,27 @@
+<?php
+
+class Zip extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->library('zip');
+	}
+
+	function index()
+	{ 
+		$name = 'uploads/mydata1.txt';
+		$data = 'A Data String!';
+
+		$this->zip->add_data($name, $data);
+
+		// 在你的服务器的文件夹里写.zip文件。命名为"my_backup.zip"
+		$this->zip->archive('uploads/my_backup.zip'); 
+
+		// 下载此文件到桌面，命名为"my_backup.zip"
+		$this->zip->download('my_backup.zip');
+	}
+}
+
+/* End of file ftp.php */
+/* Location: ./application/controllers/ftp.php */
