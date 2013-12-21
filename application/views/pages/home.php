c84c6831bb6c0a543d4e9f298acb8f31cf05165e
zhengmz-cygwin <zhengmz@hotmail.com>
2013-12-21 22:27:11 +0800

    update home.php

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -3,6 +3,6 @@
 <p>Output the data:</p>
 <ul>
 <?php foreach ($data_arr as $item => $value):?>
-<li><?php echo $item;?>: <?php echo $value;?></li>
+<li><?php echo $item;?>: <?php is_array($value) ? echo ul($value) : echo $value;?></li>
 <?php endforeach; ?>
 </ul>
