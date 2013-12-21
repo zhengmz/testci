c1a65f6175f852db0d51bb781459182c306ea510
zhengmz-cygwin <zhengmz@hotmail.com>
2013-12-21 22:57:14 +0800

    add smiley_view.php and smileys.php: testing smiley

---
+++ application/controllers/smileys.php
@@ -0,0 +1,25 @@
+<?php
+
+class Smileys extends CI_Controller {
+
+ function __construct()
+ {
+  parent::__construct();
+ }
+ 
+ function index()
+ {
+  $this->load->helper('smiley');
+  $this->load->library('table');
+  
+  $image_array = get_clickable_smileys('smileys/', 'comments');
+
+  $col_array = $this->table->make_columns($image_array, 8);  
+   
+  $data['smiley_table'] = $this->table->generate($col_array);
+  
+  $this->load->view('smiley_view', $data);
+ }
+ 
+}
+
