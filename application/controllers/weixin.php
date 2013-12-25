51dbf2b266cce48343f900024de3650c8b9d9120
zhengmz-cygwin <zhengmz@hotmail.com>
2013-12-25 23:01:38 +0800

    update weixin

--- application/controllers/weixin.php
+++ application/controllers/weixin.php
@@ -47,7 +47,7 @@ class Weixin extends CI_Controller {
 		if ($post_arr['type'] == 'text')
 		{
 			$respone_str = $post_arr['from'].', 你好!\n';
-			$respone_str = $respone_str.'Your msg is : '.$post_arr['Content'];
+			$respone_str = $respone_str.'Your msg is : '.$post_arr['content'];
 			$data = array(
 				'to' => $post_arr['from'],
 				'from' => $post_arr['to'],
