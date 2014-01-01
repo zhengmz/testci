7d1fa7e62187acd7ca407a2089bda75b6a3d3655
zhengmz-cygwin <zhengmz@hotmail.com>
2014-01-01 11:38:20 +0800

    add weixin/login.php

---
+++ application/views/weixin/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<title>欢迎绑定微信</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+  </head>
+  
+  <body>
+	<h1><?=heading?></h1>
+	<form action="<?=action?>" method="post">
+	<table width="95%" border="0" align="center">
+	<tr align="center"><td style="height: 40px;">
+		请输入您的账号进行绑定
+	</td></tr>
+	<tr><td><hr style="width:94%;"/></td></tr>
+	<tr align="center"><td style="height: 40px;">
+		<input name="username" type="text" style="color:#999; width:94%; height: 28px;" value="邮箱地址/手机号码/mifi ID"/>
+		<span id="txtinfo" style="color:red" ></span>
+	</td></tr>
+	<tr align="center"><td style="height: 40px;">
+		<input type="text" value="账号密码" name="pwd" style="color:#8C8C8C; width:94%; height: 28px;"/>
+		<span id="txtpwd" style="color:red;" ></span>
+	</td></tr>
+        <tr><td style="height: 40px;">
+		<div style="padding-top: 10px; padding-left: 3%; ">
+                登录即表示您同意遵守MIFI助手账户的<br/><a href="#">用户协议</a>和<a href="#">隐私设置</a>
+		</div>
+	</td></tr>
+	<tr align="center"><td style="height: 40px;">
+        	<input type="submist" style="background-color:#F60; font-size: 16px; color: #FFF; height: 36px; width: 94%;" value="绑定">
+        </td></tr>
+	<tr align="center"><td>
+		<div align="right" style="padding-top: 10px; padding-right: 3%;">
+		<a href="/wx_home/action">立即注册</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="#">忘记密码</a>
+		</div>
+	</td></tr>
+	</table>
+	</form>
+  </body>
+</html>
