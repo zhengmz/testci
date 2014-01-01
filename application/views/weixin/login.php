ff73ba71f52a02fbb6890a9a7444d7f4d98ce310
zhengmz-cygwin <zhengmz@hotmail.com>
2014-01-01 17:12:30 +0800

    update login.php

--- application/views/weixin/login.php
+++ application/views/weixin/login.php
@@ -21,37 +21,32 @@ input::-ms-reveal{display:none;}
 .last-enter-item { border-radius: 0 0 6px 6px; margin-top:-1px; _margin-top: -3px;}
 .ng-form-area {display:block!important; margin-left:40px; width: 324px; padding-top: 10px; height:330px; overflow: hidden;}
 .ng-form-area{display: none;}
+.shake-area{position: relative;}
+.enter-area {position: relative;}
+.nl-login-title{font-size: 40px; margin-bottom: 10px; color: #000;}
 </style>
   </head>
   
   <body>
+<div class="nl-login-title">请输入您的账号进行绑定 </div>
   <div class="ng-form-area">
 	<form action="<?=$action?>" method="post">
-	<table width="95%" border="0" align="center">
-	<tr align="center"><td style="height: 40px;">
-		请输入您的账号进行绑定
-	</td></tr>
-	<tr><td>&nbsp;</td></tr>
-	<tr align="center"><td style="height: 40px;">
+		
+      <div class="shake-area" id="shake_area">
+        <div class="enter-area">
 		<input class="enter-item first-enter-item" name="login" type="text" value="邮箱地址/手机号码/mifi ID" />
-	</td></tr>
-	<tr align="center"><td style="height: 40px;">
+        </div>
+        <div class="enter-area">
 		<input class="enter-item last-enter-item" type="text" value="账号密码" name="pwd" />
-	</td></tr>
-        <tr><td style="height: 40px;">
-		<div style="padding-top: 10px; padding-left: 3%; ">
+        </div>
+      </div>
+        <div class="ng-link-area">
                 登录即表示您同意遵守MIFI助手账户的<br/><a href="#">用户协议</a>和<a href="#">隐私设置</a>
-		</div>
-	</td></tr>
-	<tr align="center"><td style="height: 40px;">
+	</div>
       		<input class="button orange" type="submit" value="绑定" />
-        </td></tr>
-	<tr><td>
-		<div align="right" style="padding-top: 10px; padding-right: 3%;">
+        <div align="right" class="ng-link-area">
 		<a href="/wx_home/action">立即注册</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="#">忘记密码</a>
-		</div>
-	</td></tr>
-	</table>
+	</div>
 	</form>
   </div>
   </body>
