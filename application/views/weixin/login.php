d78af05f211ec67d7bc3c1bcd08f0c2470c63501
zhengmz-cygwin <zhengmz@hotmail.com>
2014-01-01 16:53:42 +0800

    update login.php

--- application/views/weixin/login.php
+++ application/views/weixin/login.php
@@ -8,8 +8,17 @@ html,body,ul,li,p,h1,div,a,img{padding: 0; margin: 0;}
 html,body{background:#f9f9f9; line-height: 1.5; color: #333; font-size: 14px;}
 body,input{font-family: "Hiragino Sans GB", "Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif;}
 .button {cursor:pointer; background: #fff; border: 1px solid #e5e5e5; border-radius: 6px; color: #7c7c7c; display: block; font-size: 16px; height: 44px; line-height: 44px; text-align: center; width: 320px;}
+.button:hover {color: #333;}
+.button {margin: 16px 0 0 0;}
 input::-ms-clear{display:none;}
 input::-ms-reveal{display:none;}
+.orange {background: #ff7a4d; color: #fff; border: 1px solid #ff7549;}
+.orange:hover {color:#fff; background: #ff936a; border: 1px solid #ff936a;}
+.orange:active {color:#fff; background: #f6683b; border: 1px solid #f6683b;}
+.enter-item {background: #fff; width: 298px; display: block; height: 20px; line-height: 20px; padding: 12px 10px; border: 1px solid #e6e6e6; border-radius: 6px; color: #333; font-size: 14px; font-weight: bold;}
+.enter-item:focus {box-shadow: inset 0 0 8px #eaeaea; background: #fdfdfd;}
+.first-enter-item {border-radius: 6px 6px 0 0;}
+.last-enter-item { border-radius: 0 0 6px 6px; margin-top:-1px; _margin-top: -3px;}
 </style>
   </head>
   
@@ -17,14 +26,14 @@ input::-ms-reveal{display:none;}
 	<form action="<?=$action?>" method="post">
 	<table width="95%" border="0" align="center">
 	<tr align="center"><td style="height: 40px;">
-		<font size="14px">请输入您的账号进行绑定</font>
+		请输入您的账号进行绑定
 	</td></tr>
 	<tr><td>&nbsp;</td></tr>
 	<tr align="center"><td style="height: 40px;">
-		<input class="login" name="login" type="text" value="邮箱地址/手机号码/mifi ID">
+		<input class="enter-item first-enter-item" name="login" type="text" value="邮箱地址/手机号码/mifi ID" />
 	</td></tr>
 	<tr align="center"><td style="height: 40px;">
-		<input class="pwd" type="text" value="账号密码" name="pwd" >
+		<input class="enter-item last-enter-item" type="text" value="账号密码" name="pwd" />
 	</td></tr>
         <tr><td style="height: 40px;">
 		<div style="padding-top: 10px; padding-left: 3%; ">
@@ -32,7 +41,7 @@ input::-ms-reveal{display:none;}
 		</div>
 	</td></tr>
 	<tr align="center"><td style="height: 40px;">
-        	<input class="btn" type="submist" value="绑定">
+      		<input class="button orange" type="submit" value="绑定" />
         </td></tr>
 	<tr><td>
 		<div align="right" style="padding-top: 10px; padding-right: 3%;">
