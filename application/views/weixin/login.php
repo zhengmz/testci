b12d9c7d62f5210601facf47b7e3a822fe5facdd
zhengmz-cygwin <zhengmz@hotmail.com>
2014-01-01 22:43:24 +0800

    update login.php

--- application/views/weixin/login.php
+++ application/views/weixin/login.php
@@ -45,5 +45,13 @@ a:hover {color: red;}
 	</div>
 	</form>
   </div>
+<script type="text/javascript">
+var browser=navigator.appName
+var b_version=navigator.appVersion
+var version=parseFloat(b_version)
+document.write("浏览器名称："+ browser)
+document.write("<br />")
+document.write("浏览器版本："+ version)
+</script>
   </body>
 </html>
