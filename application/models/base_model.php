cd505f81788146e338d989cba0c71b8a891960ba
zhengmz-centos6 <zhengmz@139.com>
2014-01-03 12:01:55 +0800

    add base_model.php

---
+++ application/models/base_model.php
@@ -0,0 +1,8 @@
+<?php  if ( ! defined('APPPATH')) exit('No direct script access allowed');
+
+class Base_model extends MY_Model {
+
+}
+
+/* End of file base_model.php */
+/* Location: ./application/models/base_model.php */
