32dce90ea9cbba9df49695704e2e6fd58f1f6446
zhengmz-cygwin <zhengmz@hotmail.com>
2014-01-04 22:16:57 +0800

    update blog_template.php

--- application/views/blog_template.php
+++ application/views/blog_template.php
@@ -6,6 +6,31 @@
 <meta http-equiv="Cache-Control" content="no-cache, must-revalidate"> 
 <meta http-equiv="expires" content="Wed, 26 Feb 1997 08:21:57 GMT">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=3" >
+
+<style>
+	.blog_body {
+		display:block!important;
+		margin-left:3%;
+		width: 94%;
+		padding-top: 2%;
+		margin-bottom:20px;
+	}
+	html,body,ul,li,p,h1,div,a,img,i,span,input,em
+	{
+		padding: 0;
+		margin: 0;
+		font-size: 16px;
+		font-family: "Hiragino Sans GB", "Microsoft YaHei", "WenQuanYi Micro Hei", "sans-serif";
+		font-weight: normal;
+		line-height: 1.5; 
+		width: 100%;
+	}
+	h1 {display:block; font-size: 20px; line-height: 2; color: black; font-weight: bold;}
+	a,label, :focus {outline: 0 none;}
+	a,img {border: 0 none;}
+	a {color: blue;}
+	a:hover {color: red;}
+</style>
 </head>
 <body>
 
@@ -13,7 +38,7 @@
 
 {blog_entries}
 <h5>{title}</h5>
-<p>{body}</p>
+<div class='blog_body'>{body}</div>
 {/blog_entries}
 </body>
 </html>
