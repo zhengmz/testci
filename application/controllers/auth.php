ff0e41c22bf01234049e25f3590bd84b9edcf30f
zhengmz-cygwin <zhengmz@hotmail.com>
2014-01-04 10:29:00 +0800

    update Qq.php

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -1,21 +1,5 @@
 <?php defined('APPPATH') OR exit('No direct script access allowed');
 
-function file_get_content($url) {
-	if (function_exists('file_get_contents')) {
-		$file_contents = @file_get_contents($url);
-	}
-	if ($file_contents == ”) {
-		$ch = curl_init();
-		$timeout = 30;
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-		$file_contents = curl_exec($ch);
-		curl_close($ch);
-	}
-	return $file_contents;
-}
-
 class Auth extends CI_Controller
 {
     public function session($provider)
--- application/libraries/oauth2/Provider/Qq.php
+++ application/libraries/oauth2/Provider/Qq.php
@@ -1,5 +1,21 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+function file_get_content($url) {
+	if (function_exists('file_get_contents')) {
+		$file_contents = @file_get_contents($url);
+	}
+	if ($file_contents == ”) {
+		$ch = curl_init();
+		$timeout = 30;
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		$file_contents = curl_exec($ch);
+		curl_close($ch);
+	}
+	return $file_contents;
+}
+
  /**
   * Oauth2 SocialAuth for CodeIgniter
   * QQ Provider
