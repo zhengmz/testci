11db822dcc9ee6287aea11c63d7f4d2a1f54ef68
zhengmz-cygwin <zhengmz@hotmail.com>
2014-01-05 21:29:52 +0800

    update cache_test.php

--- application/controllers/cache_test.php
+++ application/controllers/cache_test.php
@@ -5,7 +5,7 @@ class Cache_test extends CI_Controller {
 	function index()
 	{
 		$this->load->helper(array('form', 'url', 'html'));
-		$this->load->driver('cache');
+		$this->load->driver('cache', array('adapter' => 'file'));
 
 		$user = $this->input->post('user');
 		//$cache_user = $this->cache->file->get('user');
