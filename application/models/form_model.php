4a55cdebf2cbd892d96468d9a7d2b69495f84ee8
zhengmz-cygwin <zhengmz@hotmail.com>
2014-01-10 15:26:02 +0800

    add form_model.php

---
+++ application/models/form_model.php
@@ -0,0 +1,8 @@
+<?php  if ( ! defined('APPPATH')) exit('No direct script access allowed');
+
+class Form_model extends MY_Model {
+
+}
+
+/* End of file base_model.php */
+/* Location: ./application/models/base_model.php */
